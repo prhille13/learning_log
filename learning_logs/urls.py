1a43344b854ed9d9dc8387f723e3f541c2889d4c
prhille13 <peter.hille13@gmail.com>
2023-09-28 13:28:08 -0400

    Added views and templates

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,9 +1,9 @@
-"""Defines URL patterns for lerning_logs"""
+"""Defines URL patterns for learning_logs"""
 
 from django.urls import path
 from . import views
 
-app_name = 'lerning_logs'
+app_name = 'learning_logs'
 
 urlpatterns = [
     #Home page
