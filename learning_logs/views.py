4071bcf6c5cbe2c69b53e62bac396db6f30a1367
prhille13 <peter.hille13@gmail.com>
2023-10-20 17:18:51 -0400

    Views bug fix

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -32,7 +32,7 @@ def new_topic(request):
         if form.is_valid():
             form.save()
             return redirect('learning_logs:topics')
-        
+
     # Display a blank or invalid form
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
@@ -52,7 +52,7 @@ def new_entry(request, topic_id):
             new_entry.topic = topic
             new_entry.save()
             return redirect('learning_logs:topic', topic_id=topic_id)
-        
+
     # Display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
@@ -70,17 +70,6 @@ def edit_entry(request, entry_id):
         if form.is_valid():
             form.save()
             return redirect('learning_logs:topic', topic_id=topic.id)
-    
-<<<<<<< HEAD
+
     context = {'entry': entry, 'topic':topic, 'form':form}
     return render(request, 'learning_logs/edit_entry.html', context)
-=======
-def edit_entry(request, entry_id):
-  """Edit an existing entry."""
-  entry = Entry.objects.get(id=entry_id)
-  topic = entry.topic
-
-  if request.method != 'POST':
-    # Initial request; pre-fill form with the current entry.
-    form = EntryForm
->>>>>>> 9354768eca649fc0ac4b59c16a517cb7a2e34831
